6ba28bac6cd77952694735278b7f8b2eba4bac0c
Allyson Yamasaki <allyson.yamasaki@gmail.com>
2019-11-17 18:18:22 -0800

    test cases

--- gtest/tests1.cpp
+++ gtest/tests1.cpp
@@ -69,36 +69,131 @@ TEST(CheckPoint, CheckPoint_Add5)
 }
 
 
-//
-//TEST(PostCheckPoint, InOrderTraversal)
-//{
-//	MyAVLTree<int, std::string> tree;
-//	tree.insert(5, "foo");
-//	tree.insert(3, "sna");
-//	tree.insert(10, "bar");
-//	tree.insert(12, "twelve");
-//	tree.insert(15, "fifteen");
-//
-//	std::vector<int> trav = tree.inOrder();
-//	std::vector<int> expected = {3, 5, 10, 12, 15};
-//	EXPECT_TRUE( trav == expected );
-//}
-//
-//
-//
-//TEST(PostCheckPoint, JackSparrow)
-//{
-//	std::string quote = "I'm dishonest, and a dishonest man you can ";
-//	quote += "always trust to be dishonest. Honestly. It's the honest ";
-//	quote += "ones you want to watch out for, because you can never ";
-//	quote += "predict when they're going to do something incredibly... stupid.";
-//
-//	std::istringstream stream( quote );
-//
-//	MyAVLTree<std::string, unsigned> tree;
-//
-//	countWords(stream, tree);
-//	EXPECT_TRUE(tree.find("dishonest") == 3);
-//}
+
+TEST(PostCheckPoint, InOrderTraversal)
+{
+	MyAVLTree<int, std::string> tree;
+	tree.insert(5, "foo");
+	tree.insert(3, "sna");
+	tree.insert(10, "bar");
+	tree.insert(12, "twelve");
+	tree.insert(15, "fifteen");
+
+	std::vector<int> trav = tree.inOrder();
+	std::vector<int> expected = {3, 5, 10, 12, 15};
+	EXPECT_TRUE( trav == expected );
+}
+
+
+
+TEST(PostCheckPoint, JackSparrow)
+{
+	std::string quote = "I'm dishonest, and a dishonest man you can ";
+	quote += "always trust to be dishonest. Honestly. It's the honest ";
+	quote += "ones you want to watch out for, because you can never ";
+	quote += "predict when they're going to do something incredibly... stupid.";
+
+	std::istringstream stream( quote );
+
+	MyAVLTree<std::string, unsigned> tree;
+
+	countWords(stream, tree);
+	EXPECT_TRUE(tree.find("dishonest") == 3);
+}
+
+TEST(Finaltest, SmallBalance)
+{
+MyAVLTree<int,int> tree;
+tree.insert(5,1);
+tree.insert(3,1);
+tree.insert(2,1);
+std::vector<int> trav = tree.preOrder();
+std::vector<int> expected = {3,2,5};
+EXPECT_TRUE( trav == expected );
+}
+
+TEST(Finaltest, BigBalance)
+{
+MyAVLTree<int,int> tree;
+tree.insert(5,1);
+tree.insert(3,1);
+tree.insert(2,1);
+tree.insert(4,1);
+tree.insert(6,1);
+tree.insert(7,1);
+tree.insert(8,1);
+std::vector<int> trav = tree.preOrder();
+std::vector<int> expected = {5,3,2,4,7,6,8};
+EXPECT_TRUE( trav == expected );
+}
+
+TEST(Hamlet, HamletOpen)
+{
+
+std::ifstream stream;
+stream.open("/home/ics46b/projects/proj3/gtest/hamletopen.txt");
+
+MyAVLTree<std::string, unsigned> tree;
+countWords(stream, tree);
+stream.close();
+
+EXPECT_TRUE(tree.find("marcellus") == 9);
+}
+
+TEST(Hamlet, HamletAct1)
+{
+
+std::ifstream stream;
+stream.open("/home/ics46b/projects/proj3/gtest/hamletact1.txt");
+MyAVLTree<std::string, unsigned> tree;
+
+countWords(stream, tree);
+stream.close();
+EXPECT_TRUE(tree.find("marcellus") == 48);
+}
+
+TEST(Order, inOrder)
+{
+MyAVLTree<int, std::string> tree;
+for (int i=10;i>4;i--)
+{
+tree.insert(i,"i=");
+}
+
+std::vector keys=tree.inOrder();
+std::vector expected={5,6,7,8,9,10};
+for (int i=0;i<keys.size();i++)
+std::cout<<"at keys["<<i<<"] "<<keys[i]<<std::endl;
+EXPECT_TRUE(keys==expected);
+}
+TEST(Order, postOrder)
+{
+MyAVLTree<int, std::string> tree;
+for (int i=10;i>4;i--)
+{
+tree.insert(i,"i=");
+}
+
+std::vector<int> keys=tree.postOrder();
+std::vector<int> expected={5,6,8,10,9,7};
+
+for (int i=0;i<keys.size();i++)
+std::cout<<"at keys["<<i<<"]  "<<keys[i]<<std::endl;
+EXPECT_TRUE(keys==expected);
+}
+TEST(Order, preOrder)
+{
+MyAVLTree<int, std::string> tree;
+for (int i=10;i>4;i--)
+{
+tree.insert(i,"i=");
+}
+
+std::vector keys=tree.preOrder();
+std::vector expected={7,6,5,9,8,10};
+for (int i=0;i<keys.size();i++)
+std::cout<<"at keys["<<i<<"] "<<keys[i]<<std::endl;
+EXPECT_TRUE(keys==expected);
+}
 
 }
